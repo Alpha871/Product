5805e8c0f2b6bd00e335d099c7db8cbc43b611f0
alpah_abah <bahalphaabdoulaye40@gmail.com>
2022-12-23 18:52:18 +0100

    Adding Edit and Delete buttons

--- BulkyBook.DataAccess/Reprository/Repository.cs
+++ BulkyBook.DataAccess/Reprository/Repository.cs
@@ -5,12 +5,14 @@ using System.Linq.Expressions;
 
 namespace BulkyBook.DataAccess.Reprository
 {
+    //includeProp-"Category,CoverType"
     public class Repository<T> : IRepository<T> where T : class
     {
         private readonly DataProduct _db;
         internal DbSet<T> dbSet;
         public Repository(DataProduct db) { 
             _db= db;
+            _db.Products.Include(u => u.Category).Include(u=> u.CoverType);
             this.dbSet = _db.Set<T>();
         }
         public void Add(T item)
@@ -18,17 +20,32 @@ namespace BulkyBook.DataAccess.Reprository
             dbSet.Add(item);
         }
 
-        public IEnumerable<T> GetAll()
+        public IEnumerable<T> GetAll(string? includeProperties = null)
         {
             IQueryable<T> query = dbSet;
+            if (includeProperties != null)
+            {
+				foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+				{
+					query = query.Include(includeProp);
+				}
+			}
+            
             return query.ToList();
         }
 
-        public T GetFirstOrDefault(Expression<Func<T, bool>> filter)
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter, string? includeProperties = null)
         {
             IQueryable<T> query = dbSet;
             query = query.Where(filter);
-            return query.FirstOrDefault();
+			if (includeProperties != null)
+			{
+				foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+				{
+					query = query.Include(includeProp);
+				}
+			}
+			return query.FirstOrDefault();
         }
 
         public void Remove(T item)
